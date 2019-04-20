6730133636000a3f06aaf5d65f80fe56eed53377
Pierce Bartine <piercebartine@gmail.com>
2019-04-20 16:43:26 -0700

    Graceful shutdown on SIGTERM

--- internal/app/gravity/server.go
+++ internal/app/gravity/server.go
@@ -3,9 +3,11 @@ package gravity
 import (
 	"net/url"
 	"os"
+	"os/signal"
 	"path"
 	"strings"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/einsteinplatform/gravity/pkg/git"
@@ -17,23 +19,34 @@ import (
 // StartServer clones the provided Git repositories and begins
 // supervising the Terraform projects contained within them
 func StartServer(repoURLs []string, cloneDirBase, githubToken string) {
+	sigterm, quit := make(chan os.Signal, 2), make(chan struct{})
+	signal.Notify(sigterm, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sigterm
+		log.Debug().Msg("Signal interrupt received; cleaning up")
+		close(quit)
+	}()
+
 	var wg sync.WaitGroup
 
 	for _, repoURL := range repoURLs {
-		go superviseRepo(repoURL, cloneDirBase, githubToken, &wg)
+		go fanoutRepo(repoURL, cloneDirBase, githubToken, &wg, quit)
 		wg.Add(1)
 	}
 
 	wg.Wait()
 }
 
-func superviseRepo(repoURL, cloneDirBase, githubToken string, wg *sync.WaitGroup) {
+// fanoutRepo clones a git repo, finds its Terraform project directories,
+// and starts goroutines for each one
+func fanoutRepo(repoURL, cloneDirBase, githubToken string, wg *sync.WaitGroup, quit chan struct{}) {
 	defer wg.Done()
 
 	log.Debug().Str("repoURL", repoURL).Msg("Parsing repo URL")
 	u, err := url.Parse(repoURL)
 	if err != nil {
-		log.Fatal().Str("repoURL", repoURL).Msg("Parsing repo URL failed")
+		log.Error().Str("repoURL", repoURL).Msg("Parsing repo URL failed")
+		return
 	}
 
 	cloneDirRepo := path.Join(u.Host, strings.TrimSuffix(u.Path, ".git"))
@@ -43,59 +56,86 @@ func superviseRepo(repoURL, cloneDirBase, githubToken string, wg *sync.WaitGroup
 		log.Debug().Str("repo", repoURL).Msg("Cloning")
 		err = git.Clone(repoURL, cloneDirFull, githubToken)
 		if err != nil {
-			log.Fatal().Str("repo", repoURL).Msg("Cloning failed")
+			log.Error().Str("repo", repoURL).Msg("Cloning failed")
+			return
 		}
 	} else {
 		log.Debug().Str("repo", cloneDirRepo).Msg("Repo exits; pulling")
 		err = git.Pull(cloneDirFull, githubToken)
 		if err != nil {
-			log.Fatal().Str("repo", cloneDirRepo).Msg("Pulling failed")
+			log.Error().Str("repo", cloneDirRepo).Msg("Pulling failed")
+			return
 		}
 	}
 
 	log.Debug().Str("repo", cloneDirRepo).Msg("Searching for Terraform backend directories")
 	backendDirs, err := terraform.FindStatefulDirs(cloneDirFull)
 	if err != nil {
-		log.Fatal().Str("repo", cloneDirRepo).Msg("Backend dir search failed")
+		log.Error().Str("repo", cloneDirRepo).Msg("Backend dir search failed")
+		return
 	}
 
 	log.Debug().Str("backendDirs", "["+strings.Join(backendDirs, " ")+"]").Msg("Found backend directories")
 	for _, backendDir := range backendDirs {
-		go superviseBackend(backendDir, wg)
+		go superviseBackend(backendDir, wg, quit)
 		wg.Add(1)
 	}
 }
 
-func superviseBackend(backendDir string, wg *sync.WaitGroup) {
+// TODO fanoutBackend() -> superviseWorkspaces()
+
+func superviseBackend(backendDir string, wg *sync.WaitGroup, quit chan struct{}) {
 	defer wg.Done()
 
 	log.Debug().Str("backendDir", backendDir).Msg("Running Terraform init")
 	_, err := terraform.Init(backendDir)
 	if err != nil {
-		log.Fatal().Str("backendDir", backendDir).Msg("Init failed")
+		log.Error().Str("backendDir", backendDir).Msg("Init failed")
+		return
 	}
 
+planLoop:
 	for {
 		log.Debug().Str("backendDir", backendDir).Msg("Running Terraform plan")
 		planOut, err := terraform.Plan(backendDir)
 		if err != nil {
 			log.Error().Str("backendDir", backendDir).Msg("Plan failed")
-			continue
+			return
 		}
 
 		if !strings.Contains(*planOut, "No changes. Infrastructure is up-to-date.") {
 			log.Info().Str("backendDir", backendDir).Msg("Drift detected! Running Terraform apply")
 			_, err := terraform.Apply(backendDir)
 			if err != nil {
-				log.Fatal().Str("backendDir", backendDir).Msg("Apply failed")
+				log.Error().Str("backendDir", backendDir).Msg("Apply failed")
+				return
 			}
 			log.Info().Str("backendDir", backendDir).Msg("Apply succeeded")
 		} else {
 			log.Debug().Str("backendDir", backendDir).Msg("No changes")
 		}
 
-		time.Sleep(time.Duration(viper.GetInt("default_period")) * time.Second)
+		// TODO: need to randomize when the goroutines run, else API throttling will occur
+		wake := makeSleepChan(viper.GetInt("default_period"))
+
+		select {
+		case <-quit:
+			break planLoop
+		case <-wake:
+			continue planLoop
+		}
 	}
+
+	log.Debug().Str("backendDir", backendDir).Msg("Ending backend supervision")
 }
 
-// TODO: if signal ctrl-c received, graceful shutdown -> break
+// makeSleepChan creates a channel that will close after the provided
+// number of seconds
+func makeSleepChan(seconds int) chan struct{} {
+	wake := make(chan struct{})
+	go func() {
+		time.Sleep(time.Duration(seconds) * time.Second)
+		close(wake)
+	}()
+	return wake
+}
